331219a2543df576505b5222240a441e9b0997ef
Asila4ahmed <114068233+Asila4ahmed@users.noreply.github.com>
2023-03-18 08:54:35 +0100

    Create ERC20-2.sol

--- ERC20-2.sol
+++ ERC20-2.sol
@@ -1,6 +1,18 @@
 pragma solidity ^0.8.0;
 
- import "./ierc.sol";
+ interface IERC20{
+     function totalSupply() external view returns(uint);
+     function balanceOf(address acount) external view returns(uint);
+     function transfer(address recipient, uint amount) external returns(bool);
+     function allowance(address owner, address spender) external returns(uint);
+     function approve(address spender, uint amount) external returns(bool);
+     function transferFrom(address owner, address recipient, uint amount)external returns(bool);
+
+     event Transfer(address indexed from, address indexed to, uint amount);
+     event Approval(address indexed owner, address indexed sender, uint amount);
+
+
+ }
 
  contract ERC20 is IERC20{
      uint public override totalSupply; //override is used to avoid repeatition
